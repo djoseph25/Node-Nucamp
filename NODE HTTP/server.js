db75a919575811d148d5aac6f6e14f62067f602a
djoseph25 <59910157+djoseph25@users.noreply.github.com>
2020-10-16 21:39:52 -0400

    serverfinish.js

--- NODE HTTP/server.js
+++ NODE HTTP/server.js
@@ -3,15 +3,50 @@ const http = require('http');
 const hostname = 'localhost';
 const port = 3000;
 
+//NOTE path and fs module are both core module in node there no need to install them.
+
+
+const path = require('path');
+const fs = require('fs');
+
 const server = http.createServer((req, res) => {
-	console.log(req.headers);
-	//NOTE 200 mean everything Ok
-	res.statusCode = 200;
-	res.setHeader('Content-type', 'text/html');
-	//NOTE if my html is short i can just send in my res.end()
-	res.end(`<html><body><h1>HEllo World!</h1></body></html>`);
+    console.log(`Request for ${req.url} by method ${req.method}`);
+
+    if (req.method === 'GET') {
+        let fileUrl = req.url;
+        if (fileUrl === '/') {
+            fileUrl = '/index.html';
+        }
+
+        const filePath = path.resolve('./public' + fileUrl);
+        const fileExt = path.extname(filePath);
+
+        if (fileExt === '.html') {
+			//NOTE fs.access check to see if a file even exist
+            fs.access(filePath, err => {
+                if (err) {
+                    res.statusCode = 404;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(`<html><body><h1>Error 404: ${fileUrl} not found</h1></body></html>`);
+                    return;
+                }
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+				//NOTE fs.createReadStream it does lazy loading one piece at a time
+                fs.createReadStream(filePath).pipe(res);
+            });
+        } else {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/html');
+            res.end(`<html><body><h1>Error 404: ${fileUrl} is not an HTML file</h1></body></html>`);
+        }
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end(`<html><body><h1>Error 404: ${req.method} not supported</h1></body></html>`);
+    }
 });
 
 server.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname}:${port}/`)
-})
+	console.log(`Server running at http://${hostname}:${port}/`);
+});
