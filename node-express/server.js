91ead8077fbff98538376e314873e9c507e9dfcf
djoseph25 <59910157+djoseph25@users.noreply.github.com>
2020-10-17 12:08:04 -0400

    express server routing
    Morgan added I able to route to campsites and cmapsite.id added get post and put method

--- node-express/server.js
+++ node-express/server.js
@@ -1,22 +1,65 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-const hostname = 'localhost'
+const hostname = 'localhost';
 const port = 3000;
 
-const app = express()
-app.use(morgan('dev'))
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
 
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+});
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
 // NOTE Morgan will log the file for me now
 //ANCHOR _dirname will go to the obsolut path
-app.use(express.static(__dirname + '/public'))
 
+app.use(express.static(__dirname + '/public'));
 
-app.use((req, res)=>{
+app.use((req, res) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html')
-    res.end(`<html><body><h1>This is an express Server</h1></body></html>`)
-})
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
+
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-})
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
